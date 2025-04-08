851b25da65fe9d6494c691c3cb3955a68b2584fa
Raj Kundu <rjk368@gmail.com>
2025-04-08 15:37:41 -0400

    clean up preprocessing functions

--- models.js
+++ models.js
@@ -36,12 +36,12 @@ class ONNXModel {
 
 const inferenceSessionOptions = {
     // NOTE! WebGL doesn't support dynamic input shapes (e.g., variable batch size).
-    // adding "freeDimensionOverrides: { batch_size: 1 }" to the options list doesn't seem to work.
+    // adding "freeDimensionOverrides: { batch_size: 1 }" to the options list doesn't help.
     // See https://onnxruntime.ai/docs/tutorials/mobile/helpers/make-dynamic-shape-fixed.html
     // and https://github.com/microsoft/onnxruntime/issues/13909
     executionProviders: [
         'webgpu',
-        // 'webgl', // disabled; see note above
+        // 'webgl', // disabled; see above
         'cpu'
     ]
 }
@@ -56,9 +56,6 @@ const MODELS = [
         },
         preprocess: async function(input_tensor) {
             var processed_tensor = input_tensor;
-            if (processed_tensor.shape.length === 3) {
-                processed_tensor = processed_tensor.expandDims(0);
-            }
 
             const [cropHeight, cropWidth] = [224, 224];
 
@@ -69,8 +66,6 @@ const MODELS = [
             const xmax = xmin + cropWidth;
             const ymax = ymin + cropWidth;
 
-            console.log(xmin, ymin, xmax, ymax);
-
             // Apply center crop and resize (Add batch dimension before crop)
             processed_tensor = tf.image.cropAndResize(
                 processed_tensor,
@@ -79,12 +74,21 @@ const MODELS = [
                 [cropHeight, cropWidth]
             );
 
-            processed_tensor = processed_tensor.div(tf.scalar(255))  // Scale pixel values from [0, 255] to [0.0, 1.0]
-            .sub(tf.tensor([0.485, 0.456, 0.406]).reshape([1, 1, 1, 3])) // Normalize w/ mean
-            .div(tf.tensor([0.229, 0.224, 0.225]).reshape([1, 1, 1, 3])) // Normalize w/ std
+            // Rescale from [0, 255] to [0.0, 1.0]
+            processed_tensor = processed_tensor.div(tf.scalar(255));
+
+            // Normalize: (x - mean) / std
+            const mean = tf.tensor([0.485, 0.456, 0.406]);
+            const std = tf.tensor([0.229, 0.224, 0.225]);
+            processed_tensor = processed_tensor.sub(mean).div(std);
+
+            // Add outer batch size dimension if necessary
+            if (processed_tensor.shape.length === 3) {
+                processed_tensor = processed_tensor.expandDims(0);
+            }
 
             // Default Tensorflow shape is (batch_size * H * W * C)
-            // but model was trained in PyTorch, so use shape (batch_size * C * H * W)
+            // but model was trained in PyTorch, so convert tf tensor to shape (batch_size * C * H * W)
             processed_tensor = processed_tensor.transpose([0, 3, 1, 2]);
 
             const ortInputs = {
@@ -114,19 +118,25 @@ const MODELS = [
         },
         preprocess: async (input_tensor) => {
             var processed_tensor = input_tensor;
-            if (processed_tensor.shape.length === 3) {
-                processed_tensor = processed_tensor.expandDims(0);
-            }
 
-            processed_tensor = processed_tensor.div(tf.scalar(255))  // Scale pixel values from [0, 255] to [0.0, 1.0]
-            .sub(tf.tensor([0.2977059, 0.2977059, 0.2977059]).reshape([1, 1, 1, 3])) // Normalize w/ mean
-            .div(tf.tensor([0.26995874, 0.26995874, 0.26995874]).reshape([1, 1, 1, 3])) // Normalize w/ std
+            // Rescale from [0, 255] to [0.0, 1.0]
+            processed_tensor = processed_tensor.div(tf.scalar(255));
+
+            // Normalize: (x - mean) / std
+            const mean = tf.tensor([0.2977059, 0.2977059, 0.2977059]);
+            const std = tf.tensor([0.26995874, 0.26995874, 0.26995874]);
+            processed_tensor = processed_tensor.sub(mean).div(std);
 
             const [resizeHeight, resizeWidth] = [224, 224];
             processed_tensor = tf.image.resizeBilinear(processed_tensor, [resizeHeight, resizeWidth], false);
 
+            // Add outer batch size dimension if necessary
+            if (processed_tensor.shape.length === 3) {
+                processed_tensor = processed_tensor.expandDims(0);
+            }
+
             // Default Tensorflow shape is (batch_size * H * W * C)
-            // but model was trained in PyTorch, so use shape (batch_size * C * H * W)
+            // but model was trained in PyTorch, so convert tf tensor to shape (batch_size * C * H * W)
             processed_tensor = processed_tensor.transpose([0, 3, 1, 2]);
 
             const ortInputs = {
@@ -156,19 +166,25 @@ const MODELS = [
         },
         preprocess: async (input_tensor) => {
             var processed_tensor = input_tensor;
-            if (processed_tensor.shape.length === 3) {
-                processed_tensor = processed_tensor.expandDims(0);
-            }
 
-            processed_tensor = processed_tensor.div(tf.scalar(255))  // Scale pixel values from [0, 255] to [0.0, 1.0]
-            .sub(tf.tensor([0.1365239,  0.13651993, 0.13652335]).reshape([1, 1, 1, 3])) // Normalize w/ mean
-            .div(tf.tensor([0.10527499, 0.10530869, 0.10528071]).reshape([1, 1, 1, 3])) // Normalize w/ std
+            // Rescale from [0, 255] to [0.0, 1.0]
+            processed_tensor = processed_tensor.div(tf.scalar(255));
+
+            // Normalize: (x - mean) / std
+            const mean = tf.tensor([0.1365239,  0.13651993, 0.13652335]);
+            const std = tf.tensor([0.10527499, 0.10530869, 0.10528071]);
+            processed_tensor = processed_tensor.sub(mean).div(std);
 
             const [resizeHeight, resizeWidth] = [224, 224];
             processed_tensor = tf.image.resizeBilinear(processed_tensor, [resizeHeight, resizeWidth], false);
 
+            // Add outer batch size dimension if necessary
+            if (processed_tensor.shape.length === 3) {
+                processed_tensor = processed_tensor.expandDims(0);
+            }
+
             // Default Tensorflow shape is (batch_size * H * W * C)
-            // but model was trained in PyTorch, so use shape (batch_size * C * H * W)
+            // but model was trained in PyTorch, so convert tf tensor to shape (batch_size * C * H * W)
             processed_tensor = processed_tensor.transpose([0, 3, 1, 2]);
 
             const ortInputs = {
@@ -198,16 +214,22 @@ const MODELS = [
         },
         preprocess: async (input_tensor) => {
             var processed_tensor = input_tensor;
-            if (processed_tensor.shape.length === 3) {
-                processed_tensor = processed_tensor.expandDims(0);
-            }
 
-            processed_tensor = processed_tensor.div(tf.scalar(255))  // Scale pixel values from [0, 255] to [0.0, 1.0]
-            .sub(tf.tensor([0.4014854, 0.4014854, 0.4014854]).reshape([1, 1, 1, 3])) // Normalize w/ mean
-            .div(tf.tensor([0.30258739, 0.30258739, 0.30258739]).reshape([1, 1, 1, 3])) // Normalize w/ std
+            // Rescale from [0, 255] to [0.0, 1.0]
+            processed_tensor = processed_tensor.div(tf.scalar(255));
+
+            // Normalize: (x - mean) / std
+            const mean = tf.tensor([0.4014854, 0.4014854, 0.4014854]);
+            const std = tf.tensor([0.30258739, 0.30258739, 0.30258739]);
+            processed_tensor = processed_tensor.sub(mean).div(std);
 
             const [resizeHeight, resizeWidth] = [224, 224];
             processed_tensor = tf.image.resizeBilinear(processed_tensor, [resizeHeight, resizeWidth], false);
+
+            // Add outer batch size dimension if necessary
+            if (processed_tensor.shape.length === 3) {
+                processed_tensor = processed_tensor.expandDims(0);
+            }
 
             // Default Tensorflow shape is (batch_size * H * W * C)
             // but model was trained in PyTorch, so convert tf tensor to shape (batch_size * C * H * W)
